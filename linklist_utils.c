6f70f2c95d9a7e43ed4e55e5d28bb793b84443a8
ylitsak <saqque91pl@gmail.com>
2024-09-16 18:16:53 +0300

    fixed list_add_back to more simple logic

--- linklist_utils.c
+++ linklist_utils.c
@@ -6,7 +6,7 @@
 /*   By: saylital <saylital@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 11:11:58 by saylital          #+#    #+#             */
-/*   Updated: 2024/09/16 13:22:12 by saylital         ###   ########.fr       */
+/*   Updated: 2024/09/16 18:15:52 by saylital         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,42 +22,24 @@ t_stack *new_head(t_stack *head, int argc, char **input)
 	head->prev = NULL;
 	return (head);
 }
+
 void list_add_back(t_stack **current, int argc, char **input)
 {
 	t_stack *new_node;
 	int i;
 
 	new_node = NULL;
-	if (argc == 2)
+	i = 1;
+	while (input[i])
 	{
-		i = 1;
-		while (input[i] != NULL)
-		{
-			new_node = (t_stack *)malloc(sizeof(t_stack));
-			if (new_node == NULL)
-				error_and_free(argc, input, NULL);
-			new_node->value = ft_atoi(input[i]);
-			new_node->next = NULL;
-			new_node->prev = *current;
-			(*current)->next = new_node;
-			*current = new_node;
-			i++;
-		}
-	}
-	else if (argc > 2)
-	{
-		i = 1;
-		while (i < argc - 1)
-		{
-			new_node = (t_stack *)malloc(sizeof(t_stack));
-			if (new_node == NULL)
-				error_and_free(argc, input, NULL);
-			new_node->value = ft_atoi(input[i]);
-			new_node->next = NULL;
-			new_node->prev = *current;
-			(*current)->next = new_node;
-			*current = new_node;
-			i++;
-		}
+		new_node = (t_stack *)malloc(sizeof(t_stack));
+		if (new_node == NULL)
+			error_and_free(argc, input, NULL);
+		new_node->value = ft_atoi(input[i]);
+		new_node->next = NULL;
+		new_node->prev = *current;
+		(*current)->next = new_node;
+		*current = new_node;
+		i++;
 	}
 }
